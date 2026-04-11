e45b5df110598098c61b0f57b476c7e7d51ad6af
Prashant <ps5226@nyu.edu>
2026-04-11 06:15:52 -0400

    Add GRPO and PPO training scripts for Qwen2.5-3B-Instruct

---
+++ train_ppo_3b.sh
@@ -0,0 +1,18 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export N_GPUS=4
+export ROLLOUT_TP_SIZE=1
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+export WITHLENGTH=0
+export REFINEDREWARD=0
+export COARSEREWARD=0
+export STRICTMATCH=0
+export CORRECTMAX1=0
+export MAX1STEP30MAX3=0
+export SCHEDULEREWARD=0
+export SCHEDULELENGTH=0
+
+export DATA_DIR="./dataset/rlla_4k"
+export BASE_MODEL="Qwen/Qwen2.5-3B-Instruct"
+export EXPERIMENT_NAME="/app/models/toolrl-ppo-qwen-3b"
+bash ./examples/ppo_trainer/run_ppo.sh
